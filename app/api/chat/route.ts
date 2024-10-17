3e62ccc99f1444d6b3a876639e80407abc812d3c
Eddie Otudor <edbeeotudor@gmail.com>
2024-10-17 16:36:29 +0100

    Refine the prompt with a system prompt

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -9,6 +9,9 @@ export async function POST(req: Request) {
 
   const result = await streamText({
     model: cohere('command-r-plus'),
+    system: `You are a helpful assistant. Check your knowledge base before answering any questions.
+    Only respond to questions using information from tool calls.
+    if no relevant information is found in the tool calls, respond, "Sorry, I don't know."`,
     messages: convertToCoreMessages(messages),
   });
 
